589551faa2545525117a59628fcb0b4831d6d430
Zain <zain.marvi@rackspace.com>
2024-04-12 09:50:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo Appication
+
+Demo application is a simple Golang application that is used to demostrate how to integrate Argo CD driven GitOps process and CI.
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/common-nighthawk/go-figure"
+)
+
+func main() {
+	myFigure := figure.NewColorFigure("<username> is Awesome!!!", "larry3d", "yellow", true)
+	myFigure.Print()
+
+	if secret := os.Getenv("SECRET"); secret != "" {
+		mySecretFigure := figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "red", true)
+		mySecretFigure.Print()
+	}
+	
+	time.Sleep(10 * time.Hour)
+}
